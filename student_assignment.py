4108061780dfb5f642cfad7995ea548e8e463d4d
PingHungC <pinghung_chen@asus.com>
2025-02-05 16:24:22 +0800

    [DIT][GAI][HW1] Homework version: 1.0

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,18 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    pdf_loader = PyPDFLoader(q1_pdf)
+    pdf_documents = pdf_loader.load()
+    pdf_text_list = [doc.page_content for doc in pdf_documents]
+    text_splitter = CharacterTextSplitter(
+        separator="\n\n",  # 以換行符拆分
+        chunk_size=1000,   # 設定每個文本塊的最大長度
+        chunk_overlap=0, # 塊之間的重疊字數
+        length_function=len, # 用於計算長度的函數
+        is_separator_regex=False # 不使用正則表達式作為分隔符
+    )
+    pdf_texts = text_splitter.create_documents(pdf_text_list)
+    return pdf_texts[len(pdf_texts)-1]
 
 def hw02_2(q2_pdf):
     pass

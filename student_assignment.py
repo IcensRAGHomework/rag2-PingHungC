a0add2f751b8b478f927f82c44d1d880411ac299
PingHungC <pinghung_chen@asus.com>
2025-02-06 13:33:44 +0800

    [DIT][GAI][HW2] Homework version: 2.4

--- student_assignment.py
+++ student_assignment.py
@@ -2,8 +2,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
 
-q1_pdf = "OpenSourceLicenses.pdf"
-q2_pdf = "勞動基準法.pdf"
+# q1_pdf = "OpenSourceLicenses.pdf"
+# q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
@@ -21,13 +21,14 @@ def hw02_1(q1_pdf):
     return pdf_texts[len(pdf_texts)-1]
 
 def hw02_2(q2_pdf):
+    print(q2_pdf)
     pdf_loader = PyPDFLoader(q2_pdf)
     pdf_documents = pdf_loader.load()
 
     pdf_text_list = "\n".join(doc.page_content for doc in pdf_documents)
 
     text_splitter = RecursiveCharacterTextSplitter(
-        separators=[r"\s+第.+章\s+",r"\s+第.+條\s+"],
+        separators=[r"\s+第"],
         chunk_size=5,
         chunk_overlap=0,
         is_separator_regex=True
@@ -35,10 +36,10 @@ def hw02_2(q2_pdf):
     
     pdf_texts = text_splitter.split_text(pdf_text_list)
 
-    # print(len(pdf_texts))
-    # for i in range(0, len(pdf_texts)):
-    #     print("///////////////     chunk [%d]     ///////////////" % (i+1))
-    #     print(pdf_texts[i])
+    print(len(pdf_texts))
+    for i in range(0, len(pdf_texts)):
+        print("///////////////     chunk [%d]     ///////////////" % (i+1))
+        print(pdf_texts[i])
 
     return len(pdf_texts)
 
